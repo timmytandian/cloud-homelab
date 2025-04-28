b998fc5aac60261a5eab5acaf42f4d5872a0291f
pop timmytandian <m37656192+timmytandian@users.noreply.github.com>
2025-04-28 20:32:38 +0200

    add information about subnet name and nat gateway ID as output

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -17,3 +17,13 @@ output "public_subnet_id" {
   value       = aws_subnet.cloud_homelab_public.id
   description = "ID of the public subnet."
 }
+
+output "subnet_az" {
+  value       = aws_subnet.cloud_homelab_private.availability_zone
+  description = "The availability zone of this subnet."
+}
+
+output "nat_gateway_id" {
+  value       = aws_nat_gateway.cloud_homelab.id
+  description = "The ID of NAT Gateway to enable outbound internet connection for hosts in private subnet."
+}
