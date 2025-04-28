3c7a566ae85e5a23a4e8891d9baf6132fec12fe3
pop timmytandian <m37656192+timmytandian@users.noreply.github.com>
2025-04-28 20:31:47 +0200

    Modify the tailscale installation script to use exponential backoff when trying to download the Tailscale gpg key. This is used to overcome racing condition with NAT gateway provisioning.

--- terraform/scripts/install-tailscale.sh
+++ terraform/scripts/install-tailscale.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
-# Add Tailscale's package signing key and repository
-curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+# output log
+exec > >(tee /var/log/user-data.log) 2>&1  # Log to file
+echo "User data started at $(date)"
 
-# Install Tailscale
-sudo apt-get update && sudo apt-get install -y tailscale
+# some variables to perform exponential backoff in case the curl command
+# have time out because the NAT gateway is not ready yet
+MAX_ATTEMPTS=10
+ATTEMPT=0
+SUCCESS=false
 
-# Connect your machine to your Tailscale network using pre-auth-key
-sudo tailscale up --auth-key=tskey-auth-kuZrN7rHXr11CNTRL-jNs8FXSroDQbsSxM9ShXDQJ4fuJ5XrgL
+# Test connection with exponential backoff
+while [ $ATTEMPT -lt $MAX_ATTEMPTS ] && [ "$SUCCESS" = false ]; do
+  ATTEMPT=$((ATTEMPT+1))
+  echo "Attempt $ATTEMPT to connect to internet..."
+  
+  if curl --max-time 30 -s https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg > /dev/null; then
+    echo "Connection successful!"
+    SUCCESS=true
+  else
+    echo "Connection failed. Waiting before retry..."
+    SLEEP_TIME=$((2**ATTEMPT))
+    sleep $SLEEP_TIME
+  fi
+done
+
+# Install Tailscale if the connection is good
+if [ "$SUCCESS" = true ]; then
+  # Add Tailscale's package signing key and repository
+  curl -fsSL --connect-timeout 300 -v https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+  curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+  # Install Tailscale
+  sudo apt-get update && sudo apt-get install -y tailscale
+
+  # Connect your machine to your Tailscale network using pre-auth-key
+  sudo tailscale up --auth-key=tskey-auth-kBUrRBa7xS11CNTRL-DwTJzUNNHVAs5NiPpbwPUAegJek3kNsc
+else
+  echo "Failed to establish connection after $MAX_ATTEMPTS attempts"
+  exit 1
+fi
